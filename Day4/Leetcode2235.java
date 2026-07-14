9c6af88ebe0558c0eca2d0b09776547d9299780e
dhineshking200 <dhineshking200@gmail.com>
2026-07-15 02:44:02 +0530

    Initial

---
+++ Day4/Leetcode2235.java
@@ -0,0 +1,11 @@
+public class Leetcode2235 {
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("=== LeetCode 2235: Add Two Integers ===");
+        System.out.println("sum(-10, 4) = " + sum(-10, 4));
+        System.out.println("sum(12, 5) = " + sum(12, 5));
+    }
+}
