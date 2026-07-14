8c0057be83adc0e75118cea00c98abe6d22e389b
dhineshking200 <dhineshking200@gmail.com>
2026-07-15 02:45:51 +0530

    Initial

---
+++ Day4/Leetcode258.java
@@ -0,0 +1,14 @@
+public class Leetcode258 {
+    public static int addDigits(int num) {
+        if (num == 0) return 0;
+        return 1 + (num - 1) % 9;
+    }
+
+    public static void main(String[] args) {
+        int[] tests = {38, 0, 12345, 99};
+        System.out.println("=== LeetCode 258: Add Digits ===");
+        for (int n : tests) {
+            System.out.println("num = " + n + " -> Digital root: " + addDigits(n));
+        }
+    }
+}
