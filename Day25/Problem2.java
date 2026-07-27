d4efdf396b25ec7a18efff043cacdb939590c529
dhineshking200 <dhineshking200@gmail.com>
2026-07-27 22:48:20 +0530

    Initial

---
+++ Day25/Problem2.java
@@ -0,0 +1,15 @@
+package Day25;
+
+public class Problem2 {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA, b = headB;
+
+        while (a != b) {
+            a = (a == null) ? headB : a.next;
+            b = (b == null) ? headA : b.next;
+        }
+
+        return a;
+    }
+}
